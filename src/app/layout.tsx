fd1082ede76569f1d8203774a17df7bcce32d65f
kilnonedre <1600513615@qq.com>
2023-08-24 14:25:56 +0800

    ci: 注入全局样式及字体图标的引入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,7 @@
 
 import './globals.scss'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import { NextUIProvider } from '@nextui-org/react'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <NextUIProvider>{children}</NextUIProvider>
       </body>
     </html>
